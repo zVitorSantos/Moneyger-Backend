75b13615abd11cf9ecc99d688c41a49ff0b6431b
zVitorSantos <vitorsantos4736@gmail.com>
2024-07-13 19:07:15 -0300

    feat: adding migrations in database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,9 +3,9 @@ package database
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
-	"github.com/zVitorSantos/Moneyger-Backend/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -31,18 +31,42 @@ func Init() *gorm.DB {
 		log.Fatal("Failed to connect to database: ", err)
 	}
 
-	// Auto migrate schemas
-	err = db.AutoMigrate(
-		&models.Transaction{},
-		&models.DebtLoan{},
-		&models.Category{},
-		&models.Account{},
-		&models.Budget{},
-		&models.User{},
-	)
+	// Run migrations
+	err = runMigrations(db)
 	if err != nil {
 		log.Fatal("Failed to migrate database schema: ", err)
 	}
 
 	return db
+}
+
+func runMigrations(db *gorm.DB) error {
+	// Path to migrations directory
+	migrationsDir := "./internal/database/migrations"
+
+	// Find all migration files in the directory
+	err := filepath.Walk(migrationsDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".up.sql" {
+			// Read SQL migration file
+			sqlFile, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			// Execute SQL migration
+			err = db.Exec(string(sqlFile)).Error
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
