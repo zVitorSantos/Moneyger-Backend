f1aed98f03ebb6db0198cf764939b32de757bd01
zVitorSantos <vitorsantos4736@gmail.com>
2024-07-13 18:46:35 -0300

    feat: working migration order and code comments

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,35 +1,48 @@
 package database
 
 import (
-    "log"
-    "os"
-    "github.com/joho/godotenv"
-    "github.com/zVitorSantos/Moneyger-Backend/internal/models"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/zVitorSantos/Moneyger-Backend/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func Init() *gorm.DB {
-    // Load enviroment variables in the .env
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	// Load environment variables from .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    dsn := "host=" + os.Getenv("DB_HOST") +
-        " user=" + os.Getenv("DB_USER") +
-        " password=" + os.Getenv("DB_PASSWORD") +
-        " dbname=" + os.Getenv("DB_NAME") +
-        " port=" + os.Getenv("DB_PORT") +
-        " sslmode=" + os.Getenv("DB_SSLMODE")
+	// Construct DSN for PostgreSQL connection
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=" + os.Getenv("DB_SSLMODE")
 
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("failed to connect to database: ", err)
-    }
+	// Connect to PostgreSQL database
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
 
-    // Migrate schema
-    db.AutoMigrate(&models.User{}, &models.Account{}, &models.Transaction{}, &models.Category{}, &models.Budget{}, &models.DebtLoan{})
+	// Auto migrate schemas
+	err = db.AutoMigrate(
+		&models.Transaction{},
+		&models.DebtLoan{},
+		&models.Category{},
+		&models.Account{},
+		&models.Budget{},
+		&models.User{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database schema: ", err)
+	}
 
-    return db
+	return db
 }
